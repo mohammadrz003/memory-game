ea4897290338949cfec94cbac07b2a433fe330a9
mohammad rezaii <rezaiimohammad003@gmail.com>
2022-01-07 14:50:39 +0330

    show matches item and show select item

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,15 +1,93 @@
 import { useState, useEffect, useReducer } from "react";
 import MemoryItem from "./MemoryItem";
+import data from "../data.json";
 
 const initialState = {
   memoryItems: [],
-  pairsMatched: [],
+  pairsMatched: 0,
+  isMatched: { firstItem: null, secondItem: null },
   totalMoves: 0,
+  showingItems: [],
 };
 const reducer = (state, action) => {
   switch (action.type) {
-    case "setMemoryItem":
+    case "setMemoryItem": {
       return { ...state, memoryItems: action.value };
+    }
+    case "selectItem": {
+      let cloneState = { ...state };
+      const data = action.value;
+
+      if (cloneState.isMatched.firstItem === null) {
+        return {
+          ...state,
+          isMatched: { firstItem: data, secondItem: null },
+          showingItems: [...state.showingItems, data.id],
+        };
+      } else if (
+        cloneState.isMatched.firstItem !== null &&
+        cloneState.isMatched.secondItem === null
+      ) {
+        const firstItemObject = cloneState.isMatched.firstItem;
+        const match = firstItemObject.value === data.value ? true : false;
+
+        if (firstItemObject.id !== data.id) {
+          const isIdInShowingItems = state.showingItems.find((itemId) => {
+            return itemId === data.id;
+          });
+          if (isIdInShowingItems) {
+            return state;
+          } else if (!isIdInShowingItems && match) {
+            return {
+              ...state,
+              isMatched: {
+                firstItem: null,
+                secondItem: null,
+              },
+              totalMoves: cloneState.totalMoves + 1,
+              pairsMatched: match
+                ? cloneState.pairsMatched + 1
+                : cloneState.pairsMatched,
+              showingItems: [...state.showingItems, data.id],
+            };
+          } else if (!isIdInShowingItems && !match) {
+            console.log('not match function is running');
+            let prevShowingItems = [...state.showingItems];
+            let filterShowingItems = prevShowingItems.filter((id) => {
+              return state.isMatched.firstItem.id !== id;
+            });
+            return {
+              ...state,
+              isMatched: {
+                firstItem: null,
+                secondItem: null,
+              },
+              totalMoves: cloneState.totalMoves + 1,
+              pairsMatched: match
+                ? cloneState.pairsMatched + 1
+                : cloneState.pairsMatched,
+              showingItems: filterShowingItems,
+            };
+          }
+          return {
+            ...state,
+            isMatched: {
+              firstItem: null,
+              secondItem: null,
+            },
+            totalMoves: cloneState.totalMoves + 1,
+            pairsMatched: match
+              ? cloneState.pairsMatched + 1
+              : cloneState.pairsMatched,
+          };
+        } else {
+          return state;
+        }
+      }
+
+      return state;
+    }
+
     default:
       return state;
   }
@@ -21,13 +99,14 @@ const Container = () => {
 
   const selectItemHandler = (itemNumber) => {
     console.log(itemNumber);
+    dispatch({ type: "selectItem", value: itemNumber });
   };
 
   useEffect(() => {
     let memoryItemArray = [];
     for (let i = 0; i < 8; i++) {
-      let randomNumbers = Math.round(Math.random() * 100);
-      memoryItemArray.push(randomNumbers);
+      let randomNumbers = Math.random();
+      memoryItemArray.push({ value: randomNumbers, img: data[i] });
     }
 
     const duplicateMemoryItem = memoryItemArray.reduce(
@@ -40,8 +119,13 @@ const Container = () => {
     for (let i = 0; i < duplicateMemoryItem.length; i++) {
       const randomNumber = Math.floor(Math.random() * cloneMemoryItem.length);
 
-      const selectedRandomItem = cloneMemoryItem[randomNumber];
-      items.push(selectedRandomItem);
+      const selectedRandomItem = cloneMemoryItem[randomNumber].value;
+      const selectedRandomImage = cloneMemoryItem[randomNumber].img;
+      items.push({
+        id: Math.random(),
+        value: selectedRandomItem,
+        img: selectedRandomImage,
+      });
       cloneMemoryItem.splice(randomNumber, 1);
     }
 
@@ -54,7 +138,8 @@ const Container = () => {
         {memoryState.memoryItems.map((element, index) => {
           return (
             <MemoryItem
-              key={index}
+              key={element.id}
+              memoryState={memoryState}
               item={element}
               onSelectItem={() => selectItemHandler(element)}
             />
@@ -63,10 +148,12 @@ const Container = () => {
       </div>
       <div className="flex justify-between pt-4 text-gray-300">
         <h4>
-          Pairs matched: <span className="text-primary">0</span>
+          Pairs matched:{" "}
+          <span className="text-primary">{memoryState.pairsMatched}</span>
         </h4>
         <h4>
-          Total Moves: <span className="text-primary">1</span>
+          Total Moves:{" "}
+          <span className="text-primary">{memoryState.totalMoves}</span>
         </h4>
       </div>
     </div>
--- src/components/MemoryItem.jsx
+++ src/components/MemoryItem.jsx
@@ -1,10 +1,16 @@
 const MemoryItem = (props) => {
+  const isShowing = props.memoryState.showingItems.find((id) => {
+    return id === props.item.id;
+  });
+
   return (
     <div
       onClick={props.onSelectItem}
       className="flex items-center justify-center text-2xl text-white rounded-md cursor-pointer w-28 h-28 bg-secondery-100"
     >
-      {props.item}
+      {/* {props.item.value.toFixed(4)} */}
+      {isShowing ? <img className="w-3/5" src={props.item.img} alt="" /> : <h3 className="text-white text-base">Not Selected</h3>}
+      
     </div>
   );
 };
